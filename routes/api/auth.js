9b054ed1a67cdb64d0094c57e63b63db4d132737
zusoomro <zusoomro@seas.upenn.edu>
2020-03-14 17:19:22 -0400

    reduced jwt token expiry time for deployment

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -69,7 +69,7 @@ router.post(
       jwt.sign(
         payload,
         config.get('jwtSecret'),
-        { expiresIn: 360000 },
+        { expiresIn: 900 },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
