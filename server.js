3e404b92379060069be14b87f300b1540e482cb1
zusoomro <zusoomro@seas.upenn.edu>
2020-03-14 17:14:07 -0400

    production modifications

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const connectDB = require('./config/db');
 const bodyParse = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
+
 mongoose.set('useFindAndModify', false);
 
 // connect database
 connectDB();
-
-app.get('/', (req, res) => res.send('API Running'));
 
 // Use bodyparser
 app.use(bodyParse.json());
@@ -19,6 +19,16 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profiles', require('./routes/api/profiles'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
